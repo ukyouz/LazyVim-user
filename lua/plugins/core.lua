0ed10823185cb0622eced0ab8df6fbb86599305a
Chihhung Cheng <chihhung_cheng@tel.com>
2024-02-27 09:34:33 +0900

    syntax-highlighted-cursor: reduce color conflict

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -4,12 +4,30 @@ return {
         config = function()
             vim.g.onedark_style = "darker"
             vim.cmd.colorscheme("onedark")
+
+            vim.cmd([[hi Visual cterm=reverse gui=reverse]])
         end,
     },
     {
         "ukyouz/syntax-highlighted-cursor.nvim",
+        -- enabled = false,
         config = function()
             require("syntax-highlighted-cursor").setup()
+
+            local function clampColor(component)
+                return math.min(math.max(component, 0), 255)
+            end
+
+            function tuneColor(color, shift)
+                local r = math.floor(color / 0x10000) + shift
+                local g = (math.floor(color / 0x100) % 0x100) + shift
+                local b = (color % 0x100) + shift
+                return clampColor(r) * 0x10000 + clampColor(g) * 0x100 + clampColor(b)
+            end
+            -- darken the Search background to make cursor visible even at highlighted yellow text
+            local hi = vim.api.nvim_get_hl(0, {name = "Search"})
+            hi.bg = tuneColor(hi.bg, -50)
+            vim.api.nvim_set_hl(0, "Search", hi)
         end,
     },
     {
