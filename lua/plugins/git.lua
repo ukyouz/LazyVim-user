a6e712234045d1b0bcf2235591eb1a22dd385edf
231814 <chihhung.cheng@tel.com>
2024-04-15 11:26:19 +0900

    add: lazygit, vim-fugitive

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -42,4 +42,31 @@ return {
             },
         },
     },
+    {  
+        "kdheepak/lazygit.nvim",
+        cmd = {
+            "LazyGit",
+            "LazyGitConfig",
+            "LazyGitCurrentFile",
+            "LazyGitFilter",
+            "LazyGitFilterCurrentFile",
+        },
+        keys = {
+            {
+                "<leader>gg", "<cmd>:LazyGit<cr>",
+                desc = "LazyGit", noremap = false,
+            },
+        },
+        -- optional for floating window border decoration
+        dependencies =  {
+            "nvim-telescope/telescope.nvim",
+            "nvim-lua/plenary.nvim"
+        },
+        config = function()
+            require("telescope").load_extension("lazygit")
+        end,
+    },
+    {
+        "tpope/vim-fugitive",
+    },
 }
