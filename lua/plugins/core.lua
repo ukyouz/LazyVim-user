0bd49982fcc8a788121dff2de17839a100e0f58e
231814 <chihhung.cheng@tel.com>
2024-07-24 16:56:54 +0900

    add plugin: nvim-scrollbar

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -103,6 +103,7 @@ return {
                         "diagnostics",
                     },
                 },
+                lualine_y = {},
             },
             inactive_sections = {
                 lualine_b = {},
@@ -129,6 +130,47 @@ return {
                 },
             },
         },
+    },
+    {
+        "petertriho/nvim-scrollbar",
+        opts = {
+            show_in_active_only = false,
+            marks = {
+                Search = { highlight = "IncSearch" },
+            },
+        },
+        init = function(_, opts)
+            require("scrollbar.handlers.gitsigns").setup(opts)
+            require("scrollbar.handlers.search").setup({
+                override_lens = function(render, posList, nearest, idx, relIdx)
+                    local sfw = vim.v.searchforward == 1
+                    local indicator, text, chunks
+                    local absRelIdx = math.abs(relIdx)
+                    if absRelIdx > 1 then
+                        indicator = ('%d%s'):format(absRelIdx, sfw ~= (relIdx > 1) and 'N' or 'n')
+                    elseif absRelIdx == 1 then
+                        indicator = sfw ~= (relIdx == 1) and '▲' or '▼'
+                    else
+                        indicator = ''
+                    end
+
+                    local lnum, col = unpack(posList[idx])
+                    if nearest then
+                        local cnt = #posList
+                        if indicator ~= '' then
+                            text = ('%s'):format(indicator)
+                        else
+                            text = ''
+                        end
+                        chunks = {{' '}, {text, 'HlSearchLensNear'}}
+                    else
+                        text = ('%s'):format(indicator)
+                        chunks = {{' '}, {text, 'HlSearchLens'}}
+                    end
+                    render.setVirt(0, lnum - 1, col - 1, chunks, nearest)
+                end,
+            })
+        end,
     },
     {
         "mrjones2014/smart-splits.nvim",
