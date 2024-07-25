111f6dfcbf011d153f52bb603acfd22101a7e66b
231814 <chihhung.cheng@tel.com>
2024-07-25 11:48:45 +0900

    fix(nvim-scrollbar): conditionally run dependency setups

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -140,36 +140,40 @@ return {
             },
         },
         init = function(_, opts)
-            require("scrollbar.handlers.gitsigns").setup(opts)
-            require("scrollbar.handlers.search").setup({
-                override_lens = function(render, posList, nearest, idx, relIdx)
-                    local sfw = vim.v.searchforward == 1
-                    local indicator, text, chunks
-                    local absRelIdx = math.abs(relIdx)
-                    if absRelIdx > 1 then
-                        indicator = ('%d%s'):format(absRelIdx, sfw ~= (relIdx > 1) and 'N' or 'n')
-                    elseif absRelIdx == 1 then
-                        indicator = sfw ~= (relIdx == 1) and '▲' or '▼'
-                    else
-                        indicator = ''
-                    end
-
-                    local lnum, col = unpack(posList[idx])
-                    if nearest then
-                        local cnt = #posList
-                        if indicator ~= '' then
-                            text = ('%s'):format(indicator)
+            if H.has_plugin "gitsigns.nvim" then
+                require("scrollbar.handlers.gitsigns").setup(opts)
+            end
+            if H.has_plugin "nvim-hlslens" then
+                require("scrollbar.handlers.search").setup({
+                    override_lens = function(render, posList, nearest, idx, relIdx)
+                        local sfw = vim.v.searchforward == 1
+                        local indicator, text, chunks
+                        local absRelIdx = math.abs(relIdx)
+                        if absRelIdx > 1 then
+                            indicator = ('%d%s'):format(absRelIdx, sfw ~= (relIdx > 1) and 'N' or 'n')
+                        elseif absRelIdx == 1 then
+                            indicator = sfw ~= (relIdx == 1) and '▲' or '▼'
                         else
-                            text = ''
+                            indicator = ''
                         end
-                        chunks = {{' '}, {text, 'HlSearchLensNear'}}
-                    else
-                        text = ('%s'):format(indicator)
-                        chunks = {{' '}, {text, 'HlSearchLens'}}
-                    end
-                    render.setVirt(0, lnum - 1, col - 1, chunks, nearest)
-                end,
-            })
+
+                        local lnum, col = unpack(posList[idx])
+                        if nearest then
+                            local cnt = #posList
+                            if indicator ~= '' then
+                                text = ('%s'):format(indicator)
+                            else
+                                text = ''
+                            end
+                            chunks = {{' '}, {text, 'HlSearchLensNear'}}
+                        else
+                            text = ('%s'):format(indicator)
+                            chunks = {{' '}, {text, 'HlSearchLens'}}
+                        end
+                        render.setVirt(0, lnum - 1, col - 1, chunks, nearest)
+                    end,
+                })
+            end
         end,
     },
     {
