7d3f779172e702e3b8c69c78799a839bf1c3d81f
231814 <chihhung.cheng@tel.com>
2024-08-23 10:23:58 +0900

    refactor(nvim-hlslens): rearrange config script

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -37,6 +37,37 @@ return {
     },
     {
         "kevinhwang91/nvim-hlslens",
+        config = function(opts)
+            require("hlslens").setup({
+                override_lens = function(render, posList, nearest, idx, relIdx)
+                    local sfw = vim.v.searchforward == 1
+                    local indicator, text, chunks
+                    local absRelIdx = math.abs(relIdx)
+                    if absRelIdx > 1 then
+                        indicator = ('%d%s'):format(absRelIdx, sfw ~= (relIdx > 1) and 'N' or 'n')
+                    elseif absRelIdx == 1 then
+                        indicator = sfw ~= (relIdx == 1) and '▲' or '▼'
+                    else
+                        indicator = ''
+                    end
+
+                    local lnum, col = unpack(posList[idx])
+                    if nearest then
+                        local cnt = #posList
+                        if indicator ~= '' then
+                            text = ('%s'):format(indicator)
+                        else
+                            text = ''
+                        end
+                        chunks = {{' '}, {text, 'HlSearchLensNear'}}
+                    else
+                        text = ('%s'):format(indicator)
+                        chunks = {{' '}, {text, 'HlSearchLens'}}
+                    end
+                    render.setVirt(0, lnum - 1, col - 1, chunks, nearest)
+                end,
+            })
+        end,
     },
     {
         "mrjones2014/smart-splits.nvim",
@@ -283,35 +314,7 @@ return {
                 require("scrollbar.handlers.gitsigns").setup(opts)
             end
             if H.has_plugin "nvim-hlslens" then
-                require("scrollbar.handlers.search").setup({
-                    override_lens = function(render, posList, nearest, idx, relIdx)
-                        local sfw = vim.v.searchforward == 1
-                        local indicator, text, chunks
-                        local absRelIdx = math.abs(relIdx)
-                        if absRelIdx > 1 then
-                            indicator = ('%d%s'):format(absRelIdx, sfw ~= (relIdx > 1) and 'N' or 'n')
-                        elseif absRelIdx == 1 then
-                            indicator = sfw ~= (relIdx == 1) and '▲' or '▼'
-                        else
-                            indicator = ''
-                        end
-
-                        local lnum, col = unpack(posList[idx])
-                        if nearest then
-                            local cnt = #posList
-                            if indicator ~= '' then
-                                text = ('%s'):format(indicator)
-                            else
-                                text = ''
-                            end
-                            chunks = {{' '}, {text, 'HlSearchLensNear'}}
-                        else
-                            text = ('%s'):format(indicator)
-                            chunks = {{' '}, {text, 'HlSearchLens'}}
-                        end
-                        render.setVirt(0, lnum - 1, col - 1, chunks, nearest)
-                    end,
-                })
+                require("scrollbar.handlers.search").setup(opts)
             end
         end,
     },
