a97342b5517aee1c3ea7f02ae9615f2f03aa6422
231814 <chihhung.cheng@tel.com>
2024-10-09 12:44:08 +0900

    feat(lualine.nvim): only show first char of vim mode

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -259,6 +259,12 @@ return {
                 -- },
             },
             sections = {
+                lualine_a = {
+                    {
+                        'mode',
+                        fmt = function(str) return str:sub(1,1) end
+                    },
+                },
                 lualine_b = {},
                 lualine_c = {
                     {
