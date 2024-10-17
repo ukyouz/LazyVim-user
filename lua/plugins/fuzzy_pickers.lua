48443241eccd8f30360287b9410e0e8a87320390
231814 <chihhung.cheng@tel.com>
2024-10-17 14:03:52 +0900

    feat(telescope.nvim): fix slow find_files

--- lua/plugins/fuzzy_pickers.lua
+++ lua/plugins/fuzzy_pickers.lua
@@ -158,8 +158,8 @@ return {
                         },
                     },
                     path_display = {
-                        "smart",
-                        -- "shorten",  -- foldername with only the first char
+                        -- "smart",
+                        "shorten",  -- foldername with only the first char
                         "truncate",
                     },
                     mappings = {
@@ -245,14 +245,9 @@ return {
     --     end,
     --     build = "make",
     --     config = function()
-    --         -- You dont need to set any of these options. These are the default ones. Only
-    --         -- the loading is important
-    --         require('telescope').setup {
-
-    --         }
     --         -- To get fzf loaded and working with telescope, you need to call
     --         -- load_extension, somewhere after setup function:
-    --         -- require('telescope').load_extension('fzf')
+    --         require('telescope').load_extension('fzf')
     --     end
     -- },
     {
